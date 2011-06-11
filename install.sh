a68cdc3c86688773222e25a07689e6bef3f1aeae
William Woodall <wjwwood@gmail.com>
2011-06-10 19:21:00 -0500

    Updates for an older linux box.

--- install.sh
+++ install.sh
@@ -29,6 +29,6 @@ git submodule foreach git pull origin master
 git submodule foreach git submodule init
 git submodule foreach git submodule update
 
-setup command-t
+#setup command-t
 cd _vim/bundle/command-t
 rake make
